d2b69a3218924daaa29de1f28daff986866ed56d
Hardhik Kumar <hardhikkumar9@gmail.com>
2022-07-14 01:34:13 +0530

    water 2

---
+++ app/src/main/java/com/example/waterdelivery/Bottleactivity2.java
@@ -0,0 +1,48 @@
+package com.example.waterdelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class Bottleactivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottleactivity);
+
+        //hidden navigation bar
+        getSupportActionBar().hide();
+
+        //hidden status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Textview for WATER DELIVERY APP
+        TextView textView = findViewById(R.id.textwaterannimation);
+        textView.animate().translationX(1000).setDuration(1000).setStartDelay(2500);
+
+        // for moving to the main activity
+        Thread thread = new Thread() {
+
+            public void run() {
+                try {
+                    Thread.sleep(4000);
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                finally {
+                    Intent intent = new Intent(Bottleactivity2.this, WelcomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+
+        thread.start();
+    }
+}
